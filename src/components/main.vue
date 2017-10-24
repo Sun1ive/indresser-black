7e24dfea1a4af2fd03d2a72c8bd797a44527bdfe
Sergey <sunliveua@gmail.com>
2017-10-24 18:07:34 +0300

    0.1.2

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,15 +1,21 @@
 <template>
   <main>
-    <!-- section -->
+    <!-- app banner -->
     <app-banner></app-banner>
+    <!-- app pains -->
+    <app-pains></app-pains>
   </main>
 </template>
 
 <script>
 import banner from './banner'
+import pains from './pains'
+
+
 export default {
   components: {
-    'app-banner': banner
+    'app-banner': banner,
+    'app-pains': pains,
   }
 }
 </script>
---
+++ src/components/pains.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid>
+    <v-layout>
+      <v-flex xs12 class="text mt-5">
+        <h2>Усталость подождет</h2>
+        <p align="justify">Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы  </p>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 class="text">
+        <h2 class="text-xs-center">быть женщиной значит быть желанной</h2>
+        <p align="justify">Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы  </p>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 class="text">
+        <h2 class="text-xs-right">будь яркой, будь стильной </h2>
+        <p align="justify">Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы  </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped lang="stylus">
+.container
+  height 775px
+  background-image url('/static/pain.png')
+  margin-top -3px
+  display flex
+  flex-direction column
+  justify-content center
+  .layout
+    max-width 1030px
+    margin 0 auto
+    h2
+      font-size 2.15rem
+      color #fff
+      font-family 'OpenSans',Arial, Helvetica, sans-serif
+      text-transform uppercase
+      letter-spacing .1rem
+    p
+      font-size 1.6rem
+      color #8e8e8e
+      line-height 1.2
+      margin-bottom 0
+</style>
