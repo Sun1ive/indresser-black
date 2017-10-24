68a4a16bd78d928847cbd2b833ed5c078c2fcf2a
Sergey <sunliveua@gmail.com>
2017-10-24 12:49:06 +0300

    init fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import {
   Vuetify,
   VApp,
@@ -10,10 +10,10 @@ import {
   VGrid,
   VToolbar,
   transitions
-} from 'vuetify'
-import VueScroll from 'vue-scrollto'
-import './stylus/main.styl'
-import App from './App.vue'
+} from 'vuetify';
+import VueScroll from 'vue-scrollto';
+import './stylus/main.styl';
+import App from './App.vue';
 
 Vue.use(Vuetify, {
   components: {
@@ -27,16 +27,16 @@ Vue.use(Vuetify, {
     VToolbar,
     transitions
   }
-})
+});
 Vue.use(Scroll, {
   container: 'body',
   duration: 500,
   offset: 0,
   cancelable: true,
   easing: 'ease'
-})
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
