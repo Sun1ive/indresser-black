10e9347042588a88a0823f52fa40cbc1b9367380
Sergey <sunliveua@gmail.com>
2017-11-10 12:33:23 +0200

    0.3.6.1

--- src/components/modals/catalogForm.vue
+++ src/components/modals/catalogForm.vue
@@ -4,8 +4,8 @@
       <h3>Перейти к каталогу</h3>
     </v-layout>
     <v-layout justify-center row>
-      <form class="form">
-        <input required type="email" placeholder="Введите Ваш e-mail">
+      <form class="form" @submit.prevent="goToCatalog">
+        <input required v-model.lazy="userData.email" type="email" placeholder="Введите Ваш e-mail">
         <button class="myBtn" type="submit">Перейти</button>
         <div class="close" @click="closeForm"></div>
       </form>
@@ -14,19 +14,51 @@
 </template>
 
 <script>
-  export default {
-    methods: {
-      closeForm() {
-        this.$emit('closeForm');
+export default {
+  data() {
+    return {
+      userData: {
+        email: ''
       }
+    };
+  },
+  methods: {
+    closeForm() {
+      this.$emit('closeForm');
+    },
+    goToCatalog() {
+      Email.send(
+        `coats@indresser.com`,
+        'info@indresser.com',
+        // 'sunliveua@gmail.com',
+        'Перешел на каталог с dresses.indresser.com',
+        `Пользователь: ${this.userData.email}
+        \nПерешел на каталог`,
+        'mail.adm.tools',
+        'coats@indresser.com',
+        '3DLao3x1AC8t'
+      );
+
+      if (this.userData.email !== undefined && this.userData.email !== '') {
+        setTimeout(() => {
+          window.open('https://dresses.indresser.com/catalog.pdf', '_blank');
+        }, 250);
+      } else {
+        alert('Пожалуйста введите корректный e-mail');
+      }
+
+      this.userData.email = '';
+      this.$emit('closeForm');
     }
   }
+};
 </script>
 
 
 <style scoped lang="stylus">
-@import '/form.styl'
-  input
-    margin-right 1rem
-    padding-left 0.5rem
+@import '/form.styl';
+
+input
+  margin-right: 1rem
+  padding-left: 0.5rem
 </style>
